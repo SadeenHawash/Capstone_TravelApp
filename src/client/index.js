7083e9c93281fce73558b8a4a57e2ba208a862fe
sadeen_hawash <sadeenghawash593@gmail.com>
2025-02-28 17:27:42 +0200

    Service Worker Function

--- src/client/index.js
+++ src/client/index.js
@@ -12,9 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // For service workers
-// if ("serviceWorker" in navigator) {
-//   navigator.serviceWorker
-//     .register("/service-worker.js")
-//     .then(() => console.log("Service Worker registered."))
-//     .catch((err) => console.log("Service Worker registration failed:", err));
-// }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then(() => console.log("Service Worker registered."))
+    .catch((err) => console.log("Service Worker registration failed:", err));
+}
